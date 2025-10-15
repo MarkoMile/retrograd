9a209adb68eb32e1702c19433f7bc052580ab32f
MarkoMile <mmarkomile@gmail.com>
2025-10-15 19:45:26 +0200

    add backpropagation; add support for tanh

--- retrograd/engine.py
+++ retrograd/engine.py
@@ -2,8 +2,9 @@
 # Retrograd
 # A lightweight automatic differentiation engine.
 
+import math
 class Value:
-  def __init__(self, data, grad=0, _children=(), _op='', label=''):
+  def __init__(self, data, grad=0.0, _children=(), _op='', label=''):
     """Initialize the class with parameters.
     Args:
       data: data held in Value
@@ -14,6 +15,7 @@ class Value:
     self.data = data
     self.grad = grad
     self._prev = set(_children)
+    self._backward = lambda: None
     self.label = label
     self._op = _op
   
@@ -24,9 +26,44 @@ class Value:
   def __add__(self,other):
     other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data + other.data,_children=(self,other),_op='+')
+    def _backward():
+      self.grad = 1.0 * out.grad
+      other.grad = 1.0 * out.grad
+    out._backward = _backward
     return out
   
   def __mul__(self,other):
     other = other if isinstance(other, Value) else Value(other)
     out = Value(self.data * other.data,_children=(self,other),_op='*')
+    def _backward():
+      self.grad = other.data * out.grad
+      other.grad = self.data * out.grad
+    out._backward = _backward
     return out
+  
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+    out = Value(t,_children=(self,), _op='tanh')
+    def _backward():
+      self.grad = (1.0 - t**2) * out.grad
+    out._backward = _backward
+    return out
+  
+  def backward(self):
+    # topological order of the children graph
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    self.grad=1.0
+    for node in reversed(topo):
+      node._backward() #note this is the internal _backward function
+          
+    
